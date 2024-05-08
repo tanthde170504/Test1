2cc73b1bd86c3bbabdbb5bf6bcb81ab417ef7b8f
Đinh Ngọc Tâm <tamdinh572003@gmail.com>
2024-05-08 20:30:41 +0700

    Them package model

---
+++ ProjectSample/src/java/model/Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Student {
+    private int id;
+    
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    
+}
